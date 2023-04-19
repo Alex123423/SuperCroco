9aeb2d3f5bccb192dd525b2c746cce6797368e74
Alexey Davidenko <alexeydavidenko@icloud.com>
2023-04-19 18:02:53 +0300

    Initial Commit

---
+++ SuperCroco/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperCroco
+//
+//  Created by Alexey Davidenko on 19.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
