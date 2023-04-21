5fbf01bbf0f3b91e31044f660fddaf7b6b0d5c6f
Alexey Davidenko <alexeydavidenko@icloud.com>
2023-04-21 18:45:22 +0300

    Add teams model

---
+++ SuperCroco/Model/Teams.swift
@@ -0,0 +1,33 @@
+//
+//  Teams.swift
+//  SuperCroco
+//
+//  Created by Alexey Davidenko on 21.04.2023.
+//
+
+import Foundation
+
+struct Team {
+     let name: String
+     let avatar: String
+
+     static func getTeams() -> (firstTeam: Team, secondTeam: Team) {
+         let teams = [
+             Team(name: "Стройняшки", avatar: "food"),
+             Team(name: "Ковбои", avatar: "persons"),
+             Team(name: "Толстяки", avatar: "food"),
+             Team(name: "Панки", avatar: "persons"),
+             Team(name: "Звезды", avatar: "star")
+         ]
+
+         let shuffledTeams = teams.shuffled()
+         let firstTeam = shuffledTeams.randomElement()!
+         var secondTeam = shuffledTeams.randomElement()!
+
+         while firstTeam.name == secondTeam.name {
+             secondTeam = shuffledTeams.randomElement()!
+         }
+
+         return (firstTeam, secondTeam)
+     }
+ }
